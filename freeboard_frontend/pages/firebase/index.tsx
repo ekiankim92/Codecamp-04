4b2fb5c3a1f06f6e254e9988f1ab745f8dc15695
ekim <eunkukkim1992@gmail.com>
2021-11-25 04:09:43 +0900

    project

--- freeboard_frontend/pages/firebase/index.tsx
+++ freeboard_frontend/pages/firebase/index.tsx
@@ -3,55 +3,109 @@ import {
   getFirestore,
   addDoc,
   getDocs,
+  deleteDoc,
+  doc,
 } from "firebase/firestore/lite";
 import { firebaseApp } from "../_app";
 import { ChangeEvent, useState } from "react";
+import styled from "@emotion/styled";
+const MyInput = styled.div`
+  width: 150px;
+  height: 150px;
+  background-color: #ebeb8d;
+`;
+
+const MyInput_writer = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #5d5d8b;
+`;
+
+const MyInput_title = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #429442;
+`;
+
+const MyInput_contents = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #4e4e0c;
+`;
 
 export default function FirebasePage() {
-  //   const [writer, setWriter] = useState("");
-  //   const [title, setTitle] = useState("");
-  //   const [contents, setContents] = useState("");
+  const [myWriter, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+  const [myData, setMyData] = useState([]);
+  const [deleteData, setDeleteData] = useState([]);
 
-  const [userInfo, setUserInfo] = useState({
-    writer: "",
-    title: "",
-    contents: "",
-  });
+  //   spread 연산자
 
-  async function onClickFetch() {
-    const board = collection(getFirestore(firebaseApp), "board");
-    const result = await getDocs(board);
-    const boardId = result.docs.map((el) => el.id);
-    console.log(boardId);
-    console.log(boardId[0]);
-    console.log(boardId[1]);
-  }
+  //   const [userInfo, setUserInfo] = useState({
+  //     writer: "",
+  //     title: "",
+  //     contents: "",
+  //   });
 
+  //게시글 등록
   async function onClickSubmit() {
     const board = collection(getFirestore(firebaseApp), "board");
     await addDoc(board, {
-      ...userInfo,
+      writer: myWriter,
+      title: title,
+      contents: contents,
     });
   }
-  const onChngeInfo = (event: ChangeEvent<HTMLInputElement>) => {
-    setUserInfo({
-      ...userInfo,
-      [event.target.name]: event.target.value,
-    });
-  };
 
-  //   function userWriter(event) {
-  //     setWriter(event.target.value);
-  //     console.log(writer);
-  //   }
+  //게시글 조회
+  async function onClickFetch() {
+    const board = collection(getFirestore(firebaseApp), "board");
+    const result = await getDocs(board);
+    setMyData(result.docs.map((el) => el.data()));
+    setDeleteData(result.docs.map((el) => el.id));
+    // console.log(boardId);
+    // console.log(boardId[0]);
+    // console.log(boardId[1]);
+    console.log(result.docs);
+  }
 
-  //   function userTitle(event) {
-  //     setTitle(event.target.value);
-  //   }
+  //게시글 삭제
+  async function onClickDelete(event) {
+    const result = await doc(
+      getFirestore(firebaseApp),
+      "board",
+      event.target.id
+    );
+    deleteDoc(result);
+  }
 
-  //   function userContents(event) {
-  //     setContents(event.target.value);
+  //   spread 연산자
+
+  //   async function onClickSubmit() {
+  //     const board = collection(getFirestore(firebaseApp), "board");
+  //     await addDoc(board, {
+  //       ...userInfo,
+  //     });
   //   }
+  //   const onChngeInfo = (event: ChangeEvent<HTMLInputElement>) => {
+  //     setUserInfo({
+  //       ...userInfo,
+  //       [event.target.name]: event.target.value,
+  //     });
+  //   };
+
+  function userWriter(event) {
+    setWriter(event.target.value);
+  }
+
+  function userTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  function userContents(event) {
+    setContents(event.target.value);
+  }
 
   return (
     <>
@@ -59,13 +113,13 @@ export default function FirebasePage() {
       <br />
       <br />
       <br />
-      <input type="text" onChange={onChngeInfo} name="writer" />
+      <input type="text" onChange={userWriter} name="writer" />
       {/* <div>{userInfo.writer}</div> */}
       <br />
-      <input type="text" onChange={onChngeInfo} name="title" />
+      <input type="text" onChange={userTitle} name="title" />
       {/* <div>{userInfo.title}</div> */}
       <br />
-      <input type="text" onChange={onChngeInfo} name="contents" />
+      <input type="text" onChange={userContents} name="contents" />
       {/* <div>{userInfo.contents}</div> */}
       <br />
       <button onClick={onClickSubmit}>Click</button>
@@ -74,11 +128,16 @@ export default function FirebasePage() {
       <br />
       <br />
       <br />
-      <div>
-        <div>{}</div>
-        <div>{}</div>
-        <div>{}</div>
-      </div>
+      {myData.map((el, index) => (
+        <MyInput key={el.id}>
+          <MyInput_writer id={el.id}>{el.myWriter}</MyInput_writer>
+          <MyInput_title id={el.id}>{el.title}</MyInput_title>
+          <MyInput_contents id={el.id}>{el.contents}</MyInput_contents>
+          <button id={deleteData[index]} onClick={onClickDelete}>
+            Delete
+          </button>
+        </MyInput>
+      ))}
       <button onClick={onClickFetch}>조회하기</button>
     </>
   );
