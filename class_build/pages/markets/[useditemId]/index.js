fdb1d595ddc28dd6834776fbdad22ef3b5bcc461
ekim <eunkukkim1992@gmail.com>
2021-12-21 14:49:12 +0900

    update

--- class_build/pages/markets/[useditemId]/index.js
+++ class_build/pages/markets/[useditemId]/index.js
@@ -9,7 +9,10 @@ export default function MarketsPage(props) {
       <Head>
         <meta property="og:title" content={props.fetchUseditem.name} />
         {/* <meta property="og:url" content="http://sookheewoof.shop" /> */}
-        <meta property="og:image" content={props.fetchUseditem.images[0]} />
+        <meta
+          property="og:image"
+          content={`https://storage.googleapis.com/${props.fetchUseditem.images[0]}`}
+        />
         <meta property="og:description" content={props.fetchUseditem.remarks} />
       </Head>
       <div>안녕하세요! 상품페이지입니다!</div>
