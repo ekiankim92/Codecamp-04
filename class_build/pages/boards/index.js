334b8c6df5ad8336ce8da0cbaced3bf80427180e
ekim <eunkukkim1992@gmail.com>
2021-12-21 13:04:41 +0900

    update build:ssr

--- class_build/pages/boards/index.js
+++ class_build/pages/boards/index.js
@@ -8,12 +8,9 @@ export default function BoardsPage() {
         <meta property="og:url" content="http://sookheewoof.shop" />
         <meta
           property="og:image"
-          content="https://www.codingfactory.net/wp-content/uploads/abc.jpg"
+          content="https://w.namu.la/s/7085672eaa484a5967b0bb04bf063e432864a9e768c852796cdc0df0c07c3e511a0feeda5c4a451357aab58d45c9478bf2244840a4c361b4284ed835b36429f5b54924e2b7056375ac32a965f8fef1655c13e8ae781653edc6516a7007f9b645"
         />
-        <meta
-          property="og:description"
-          content="안녕하세요! 나의 사이트에 오신것을 환영합니다!"
-        />
+        <meta property="og:description" content="마음을 불태워라" />
       </Head>
       <div>안녕하세요! 게시판입니다!</div>
     </>
---
+++ class_build/pages/markets/[useditemId]/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import { request, gql } from "graphql-request";
+
+// this page must be doing 24/7 of yarn dev
+
+export default function MarketsPage(props) {
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content={props.fetchUsedItem.name} />
+        {/* <meta property="og:url" content="http://sookheewoof.shop" /> */}
+        <meta property="og:image" content={props.fetchUsedItem.images[0]} />
+        <meta property="og:description" content={props.fetchUsedItem.remarks} />
+      </Head>
+      <div>안녕하세요! 상품페이지입니다!</div>
+    </>
+  );
+}
+
+// getServerSideProps: this is provided from next.js so this must match exactly so that this page
+// will be doing the SSR (this page will be doing SSR only)
+
+// will be going to the props on top of this page
+
+const FETCH_USED_ITEM = gql`
+  query fetchUseditem($useditemId: ID!) {
+    fetchUseditem(useditemId: $useditemId) {
+      name
+      remarks
+      images
+    }
+  }
+`;
+
+// 밑 작업을 먼저 하고 위에 올라가서 props 로 넘겨줌
+// next.js 가 기본적으로 router 도 넣어줌
+// 그 위에 더 많은 정보를 넣어주는걸 context
+export const getServerSideProps = async (context) => {
+  const result = await request(
+    "https://backend04.codebootcamp.co.kr/graphql",
+    FETCH_USED_ITEM,
+    { useditemId: context.query.useditemId }
+  );
+
+  return {
+    // this props is mandatory
+    props: {
+      // this is just a name we create
+      fetchUsedItem: {
+        name: result.fetchUsedItem.name,
+        remarks: result.fetchUsedItem.remarks,
+        images: result.fetchUsedItem.images[0],
+      },
+    },
+  };
+};
