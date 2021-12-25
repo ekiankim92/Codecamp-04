4aad82c4716283fe61da95cb877bdc1014824b2b
ekim <eunkukkim1992@gmail.com>
2021-12-25 23:56:25 +0900

    IOS Practice

--- freeboard_mobile/App.js
+++ freeboard_mobile/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import type {Node} from 'react';
 // import * as R from 'react-native';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, TextInput} from 'react-native';
 
 const App: () => Node = () => {
   return (
     <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-      <View>
-        <Text>This is the test</Text>
-      </View>
-      <Text>Will this show in the IOS screen?</Text>
+      <ScrollView>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+        <View>
+          <Text>This is the test</Text>
+        </View>
+        <Text>Will this show in the IOS screen?</Text>
+        <View>
+          <TextInput
+            onPress={(alert = 'Hello World')}
+            placeholder="Enter your name"
+            stlye={{color: 'red'}}
+          />
+          <TextInput placeholder="Password" secureTextEntry={true} />
+        </View>
+      </ScrollView>
     </>
   );
 };
