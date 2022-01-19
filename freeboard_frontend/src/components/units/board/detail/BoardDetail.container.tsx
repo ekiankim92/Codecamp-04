61759b3c5f12c55315625a032f9870cb15060129
ekim <eunkukkim1992@gmail.com>
2022-01-19 14:42:09 +0900

    types updates

--- freeboard_frontend/src/components/units/board/detail/BoardDetail.container.tsx
+++ freeboard_frontend/src/components/units/board/detail/BoardDetail.container.tsx
@@ -10,17 +10,35 @@ import {
 import { useQuery, useMutation } from "@apollo/client";
 import { Contents, Password } from "../write/BoardWrite.styles";
 import { useState, useRef } from "react";
+import {
+  IMutation,
+  IMutationDeleteBoardArgs,
+  IMutationDislikeBoardArgs,
+  IMutationLikeBoardArgs,
+  IQuery,
+} from "../../../../commons/types/generated/types";
 
 //splice, slice 는 데이터를 일일히 가져오기 때문에 runtime error 가 생길수있음
 export default function ContainerDetailPage() {
   const router = useRouter();
 
   // 좋아요 싫어요
-  const [likeBoard] = useMutation(LIKE_BOARD);
-  const [dislikeBoard] = useMutation(DISLIKE_BOARD);
+  const [likeBoard] = useMutation<
+    Pick<IMutation, "likeBoard">,
+    IMutationLikeBoardArgs
+  >(LIKE_BOARD);
+
+  const [dislikeBoard] = useMutation<
+    Pick<IMutation, "dislikeBoard">,
+    IMutationDislikeBoardArgs
+  >(DISLIKE_BOARD);
 
   // 게시글 리스트 삭제
-  const [deleteBoard] = useMutation(DELETE_BOARD);
+  const [deleteBoard] = useMutation<
+    Pick<IMutation, "deleteBoard">,
+    IMutationDeleteBoardArgs
+  >(DELETE_BOARD);
+
   const [uploadFile] = useMutation(UPLOAD_FILE);
 
   //게시글 페이지 조회
@@ -53,13 +71,13 @@ export default function ContainerDetailPage() {
     try {
       await deleteBoard({
         variables: {
-          boardId: router.query.content,
+          boardId: String(router.query.content),
         },
       });
       alert("게시물이 삭제되었습니다.");
       router.push(`board_list`);
     } catch (error) {
-      console.log(error.message);
+      if (error instanceof Error) console.log(error.message);
     }
   }
 
@@ -68,7 +86,7 @@ export default function ContainerDetailPage() {
     try {
       const result = await likeBoard({
         variables: {
-          boardId: router.query.content,
+          boardId: String(router.query.content),
         },
         refetchQueries: [
           {
@@ -79,7 +97,7 @@ export default function ContainerDetailPage() {
       });
       console.log(result);
     } catch (error) {
-      console.log(error.message);
+      if (error instanceof Error) console.log(error.message);
     }
   }
 
@@ -88,7 +106,7 @@ export default function ContainerDetailPage() {
     try {
       const result = await dislikeBoard({
         variables: {
-          boardId: router.query.content,
+          boardId: String(router.query.content),
         },
         refetchQueries: [
           {
@@ -99,7 +117,7 @@ export default function ContainerDetailPage() {
       });
       console.log(result);
     } catch (error) {
-      console.log(error.message);
+      if (error instanceof Error) console.log(error.message);
     }
   }
 
