ddf18b7fc78857ded2d974f02dd1f5581428ef40
ekim <eunkukkim1992@gmail.com>
2022-04-07 15:51:20 +0900

    playground luggages

---
+++ playground/pages/amazon/index.tsx
@@ -0,0 +1,7 @@
+export default function AmazonPage() {
+  return (
+    <>
+      <div>Hello World</div>
+    </>
+  );
+}
---
+++ playground/pages/luggage/index.tsx
@@ -0,0 +1,6 @@
+import Luggage from "../../src/luggage/luggage.container";
+
+const LuggagePage = () => {
+  return <Luggage />;
+};
+export default LuggagePage;
